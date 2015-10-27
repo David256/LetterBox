623ad53e5d28c0d6b32b3d74c246b624c32ae5f0
David <davidborja456@gmail.com>
2015-10-27 18:06:45 -0500

    Cambio en la interfaz

--- src/interfaz/Ventana.java
+++ src/interfaz/Ventana.java
@@ -1,20 +1,54 @@
 package interfaz;
 
+import java.awt.Color;
+import java.awt.Font;
+
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 public class Ventana extends JFrame{
 	
 	uPanel elPanel = new uPanel();
+	JLabel logo = new JLabel("Letter BOX");
+	JTextArea elChat = new JTextArea("Iniciando chat..\n>>");
+	JTextField entrada = new JTextField("_"); 
+	
+	
 	
 	public Ventana(){
 		
+		elPanel.setLayout(null);
+		
+		setTitle("Letter BOX - by DaJu");
+		setSize(320,480);
+		
+		//ubicar logo
+		logo.setFont(new Font("verdana", Font.BOLD, 14));
+		logo.setBounds(105, 10, 250, 25);
+		
+		//ubicamos el chat
+		elChat.setBounds(7, 50, 290, 340);
+		elChat.setBackground(new Color(33,33,33));
+		elChat.setForeground(Color.white);
+		
+		//configuramos el entrada
+		entrada.setBounds(7,395,290,40);
+		entrada.setFont(new Font("verdana", Font.CENTER_BASELINE, 13));
+		
+		//agregar al panel
+		elPanel.add(logo);
+		elPanel.add(elChat);
+		elPanel.add(entrada);
+		
+		this.add(elPanel);
 		MostrarVentana();
 		
 	}
 
 	private void MostrarVentana() {
 		// TODO Auto-generated method stub
-		setSize(320,480);
 		setVisible(true);
 	}
 	
